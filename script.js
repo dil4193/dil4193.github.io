0e0a99520f01431f502660191c1b0ad9093635ac
dil4193 <71956504+dil4193@users.noreply.github.com>
2020-12-10 10:20:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//typing animation
+var typed = new Typed(".typing",{
+	strings: ["Software Developer","Digital Marketer"],
+	typeSpeed: 100,
+	backSpeed: 60,
+	loop: true
+	});
+//typing animation
+var typed = new Typed(".typing-2",{
+	strings: ["Software Developer","Digital Marketer"],
+	typeSpeed: 100,
+	backSpeed: 60,
+	loop: true
+	});
